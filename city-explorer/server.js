cac275d3ba52120220199996ac78971f2300dd20
Jessica Lovell <jessicaamberlovell@gmail.com>
2018-11-07 16:06:02 -0800

    add weather API call

--- city-explorer/server.js
+++ city-explorer/server.js
@@ -10,8 +10,24 @@ const app = express();
 const PORT = process.env.PORT;
 app.use(cors());
 
-app.get('/location', (request, response) => {
+app.get('/location', getLocation);
+app.get('/weather', getWeather);
+// app.get('/movies', getMovie);
+// app.get('/yelp', getYelp);
+// app.get('/meetups', getMeetup);
+// app.get('/trails', getTrail);
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+//HELPER FUNCTIONS
+function handleError (error, response) {
+  console.error(error);
+  if (response) return response.status(500).send('Sorry something went wrong.');
+}
+
+function getLocation (request, response) {
   const url  = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GOOGLE_API_KEY}`;
+
   return superagent.get(url)
     .then(result => {
       const locationResult = {
@@ -20,15 +36,21 @@ app.get('/location', (request, response) => {
         latitude: result.body.results[0].geometry.location.lat,
         longitude: result.body.results[0].geometry.location.lng
       }
-      response.send(locationResult);
+      response.send(locationResult)
     })
-    .catch(error => handleError(error))
-});
+    .catch(error => handleError(error));
+}
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+function getWeather (request, response) {
+  const url = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API}/${request.query.data.latitude},${request.query.data.longitude}`;
+  return superagent.get(url)
+    .then(result => {
+      response.send(result.body.daily.data.map(day => new Weather(day)));
+    })
+    .catch(error => handleError(error, response));
+}
 
-//HELPER FUNCTIONS
-function handleError (error, response) {
-  console.error(error);
-  if (response) return response.status(500).send('Sorry something went wrong.');
+function Weather(day) {
+  this.time = new Date(day.time * 1000).toString().slice(0, 15);
+  this.forecast = day.summary;
 }
